e8af4d69001421c59061bb36f340141101e385ff
whoisorioki <adrianorioki@gmail.com>
2022-11-25 01:15:32 -0800

    Print Decimal

--- printf_dec.c
+++ printf_dec.c
@@ -23,4 +23,25 @@ int printf_dec(va_list args)
 		last = -last;
 		i++;
 	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (i);
+}
 
