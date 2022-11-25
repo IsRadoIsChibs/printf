86e0acbbabbe48bdb82998ad2b5fd69481d41d1a
whoisorioki <adrianorioki@gmail.com>
2022-11-25 02:02:24 -0800

    MAIN

--- main.h
+++ main.h
@@ -19,3 +19,22 @@ typedef struct format
 } convert_match;
 
 int _printf(const char *format, ...);
+int printf_pointer(va_list val);
+int printf_hex_uns(unsigned long int num);
+int printf_HEX_uns(unsigned int num);
+int printf_exclusive_string(va_list val);
+int printf_HEX(va_list val);
+int printf_hex(va_list val);
+int printf_oct(va_list val);
+int printf_unsigned(va_list args);
+int printf_bin(va_list val);
+int printf_rev(va_list args);
+int printf_rot13(va_list args);
+int printf_int(va_list args);
+int printf_dec(va_list args);
+int printf_45(void);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _putchar(char c);
+
+#endif
