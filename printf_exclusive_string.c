a2afc67edc7e39563db41afae094c0ed660d1bb5
whoisorioki <adrianorioki@gmail.com>
2022-11-25 02:19:45 -0800

    Prints a String

--- printf_exclusive_string.c
+++ printf_exclusive_string.c
@@ -14,4 +14,26 @@ int printf_exclusive_string(va_list val)
 	s = va_arg(val, char *);
 	if (s == NULL)
 		s = "(null)";
-
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < 32 || s[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			len = len + 2;
+			cast = s[i];
+			if (cast < 16)
+			{
+				_putchar('0');
+				len++;
+			}
+			len = len + printf_HEX_aux(cast);
+		}
+		else
+		{
+			_putchar(s[i]);
+			len++;
+		}
+	}
+	return (len);
+}
