4a3964411de278865f3464de56517976d76ab0bd
whoisorioki <adrianorioki@gmail.com>
2022-11-25 02:42:12 -0800

    Prints the rot13ed string

--- printf_rot13.c
+++ printf_rot13.c
@@ -17,4 +17,23 @@ int printf_rot13(va_list args)
 	if (s == NULL)
 		s = "(null)";
 	for (i = 0; s[i]; i++)
+	{
+		k = 0;
+		for (j = 0; alpha[j] && !k; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				_putchar(beta[j]);
+				counter++;
+				k = 1;
+			}
+		}
+		if (!k)
+		{
+			_putchar(s[i]);
+			counter++;
+		}
+	}
+	return (counter);
+}
 
