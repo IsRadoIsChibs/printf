6dabcc5ae52bec4f74c009cd423219ee751f2659
IsRadoIsChibs <coloradochibs@gmail.com>
2022-11-25 09:55:03 +0000

    main.h

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+/**
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier
+ * @f: type pointer to function for the conversion specifier
+ */
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
+
+int _printf(const char *format, ...);
