47f086e81d4eb8371580e049239abcbfb4c5fb85
whoisorioki <adrianorioki@gmail.com>
2022-11-25 02:29:38 -0800

    Prints an Octal Number

--- printf_oct.c
+++ printf_oct.c
@@ -13,3 +13,23 @@ int printf_oct(va_list val)
 	unsigned int num = va_arg(val, unsigned int);
 	unsigned int temp = num;
 
+	while (num / 8 != 0)
+	{
+		num /= 8;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 8;
+		temp /= 8;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
