5f7a0c8d5cdcced9a1a0f2bc9513e35cf0725211
whoisorioki <adrianorioki@gmail.com>
2022-11-25 02:38:17 -0800

    Prints a Reversed string

--- printf_rev.c
+++ printf_rev.c
@@ -11,3 +11,11 @@ int printf_rev(va_list args)
 	int i;
 	int j = 0;
 
+	if (s == NULL)
+		s = "(null)";
+	while (s[j] != '\0')
+		j++;
+	for (i = j - 1; i >= 0; i--)
+		_putchar(s[i]);
+	return (j);
+}
