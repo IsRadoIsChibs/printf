742f9e07c6b180f7f6b2498871357b2c4e0b2af0
IsRadoIsChibs <coloradochibs@gmail.com>
2022-11-25 09:10:17 +0000

    3

---
+++ printf_dec.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * printf_dec - prints decimal
+ * @args: argument to print
+ * Return: number of characters printed
+ */
+int printf_dec(va_list args)
+{
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit;
+	int  i = 1;
+	int exp = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+
